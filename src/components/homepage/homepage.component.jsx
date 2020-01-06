7e2b13f627215328e16787eea71131593c62245d
dillonkmcquade <dillonkmcquade@gmail.com>
2020-01-05 19:20:32 -0500

    added mobile responsiveness

--- src/components/homepage/homepage.component.jsx
+++ src/components/homepage/homepage.component.jsx
@@ -6,6 +6,7 @@ const HomePage = () => {
     <div className="homepage">
       <h1 className="homepage-title">HI, MY NAME IS DILLON.</h1>
       <h2 className="homepage-title">I'M A FRONT-END WEB DEVELOPER</h2>
+      
     </div>
   );
 };
