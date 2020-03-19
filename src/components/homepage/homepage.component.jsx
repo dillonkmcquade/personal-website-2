f1542ff8e1e37d8db95ec69c6c8fff8d4ea9b291
Dillon <dillonkmcquade@gmail.com>
2020-03-19 16:47:18 -0400

    replaced lorem text homepage

--- src/components/homepage/homepage.component.jsx
+++ src/components/homepage/homepage.component.jsx
@@ -5,10 +5,7 @@ const HomePage = () => {
   return (
     <div className="homepage">
       <div className="profile fadeInLeft animated-2">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
-        doloribus deserunt obcaecati quas nesciunt consectetur veniam dolores
-        quisquam? Eum quod modi quos libero suscipit tempora vero atque itaque
-        ex beatae?
+        Hi my name's Dillon. <br /><br />I'm a software developer in the Montréal, QC area. <br /> <br /> I like building things with JavaScript and tinkering with computer software and linux. I'm also a traveler, backpacker, fly-fisherman, and I love cooking. <br /> 
       </div>
       <img
         src="https://i.postimg.cc/rssfWssk/resized-img.jpg"
